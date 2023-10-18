ef6fda92676d020584baa31b8af4a2d651ca9f55
Danh Nguyễn <danh010500@gmail.com>
2023-10-18 10:12:42 +0700

    #<W6>: Push laravel_inventory_init

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Models\User;
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,15 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Auth::viaRequest('jwt', static function (Request $request) {
-            $token = $request->header('Authorization', null);
-            if ($token === null) {
-                return null;
-            }
-            $token = (array) JWT::decode(str_replace('Bearer', '', $token), new Key(env('APP_KEY'), 'HS256'));
-            $user = new User();
-            $user->setRawAttributes($token);
-            return $user;
-        });
+        //
     }
 }
